bc40c2490b5cc056260b1c5851dcc9727876516a
Danang Adi Nugroho <danang13@mhs.if.its.ac.id>
2017-02-27 14:19:40 +0700

    add coba pesan

---
+++ coba read write pesan.py
@@ -0,0 +1,23 @@
+import binascii
+import bitarray
+
+ba = bitarray.bitarray()
+
+fileName = "message.txt"
+
+with open(fileName, mode='rb') as file: # b is important -> binary
+    fileContent = file.read()
+
+fileContent = bin(int(binascii.hexlify(fileContent), 16))
+# print(fileContent)
+
+bitmessage = ba.fromstring(fileContent)
+bitmessage = ba.tolist()
+print(bitmessage)
+
+n = int(bitarray.bitarray(bitmessage).tostring(),2)
+write = binascii.unhexlify('%x' % n)
+
+f = open('result.txt', 'wb')
+f.write(write)
+f.close()
