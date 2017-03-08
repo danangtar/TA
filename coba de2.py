ae4dcd9628aa74fc22480ec8400ae892545f7806
Danang Adi Nugroho <danang13@mhs.if.its.ac.id>
2017-03-08 17:22:32 +0700

    edit de

--- coba de2.py
+++ coba de2.py
@@ -2,16 +2,16 @@ import numpy as np
 import wave
 import TA.pesan
 
-spf = wave.open('Bruno_Mars_Versace_On_The_Floor.wav','r')
+spf = wave.open('Bruno_Mars_Versace_On_The_Floor.wav', 'r')
 
-#Extract Raw Audio from Wav File
+# Extract Raw Audio from Wav File
 signal = spf.readframes(-1)
 signal = np.fromstring(signal, 'uint8')
 
 size = signal.size
 
-mod = divmod(size,4)
-headlen = size-mod[1]
+mod = divmod(size, 4)
+headlen = size - mod[1]
 # print(size)
 # print(mod)
 
@@ -24,51 +24,63 @@ pair = pair.tolist()
 x = 0
 lm = []
 
-
 teks = TA.pesan.Pesan()
 teks = teks.getBinary()
 panjangteks = len(teks)
 print(teks)
 print(pair)
 
+
 def expandable(v, m, b):
     vtemp = 2 * v + b
-    if (128 <= m <= 255):
-        if (abs(vtemp) <= 2 * (255 - m)):
+    if 128 <= m <= 255:
+        if abs(vtemp) <= 2 * (255 - m):
             lm.append(0)
+            return vtemp
         else:
             changeable(v, m, b)
-    elif (0 <= m <= 127):
-        if (abs(vtemp) <= 2 * m + 1):
+    elif 0 <= m <= 127:
+        if abs(vtemp) <= 2 * m + 1:
             lm.append(0)
+            return vtemp
         else:
             changeable(v, m, b)
+
 
 def changeable(v, m, b):
     vtemp = 2 * abs(v / 2) + b
-    if (128 <= m <= 255):
-        if (abs(vtemp) <= 2 * (255 - m)):
+    if 128 <= m <= 255:
+        if abs(vtemp) <= 2 * (255 - m):
             lm.append(1)
-    elif (0 <= m <= 127):
-        if (abs(vtemp) <= 2 * m + 1):
+            return vtemp
+    elif 0 <= m <= 127:
+        if abs(vtemp) <= 2 * m + 1:
             lm.append(1)
+            return vtemp
     else:
         unchangeable()
 
+
 def unchangeable():
     lm.append(2)
+    return 0
 
 
 j = 0
 for isiquad in pair:
     i = 0
-    if(j < panjangteks):
+    if (j < panjangteks):
         while i < 1:
-            v = float(isiquad[i+1]) - float(isiquad[i])
+            v = float(isiquad[i + 1]) - float(isiquad[i])
             m = v / 2
             b = float(teks[j])
             # v.append(vtemp)
-            expandable(v, m, b)
+            vtemp = expandable(v, m, b)
+            if vtemp != 0:
+                uaksen1 = m + abs((vtemp + 1) / 2)
+                uaksen2 = m - abs(vtemp / 2)
+
+                pair[j] = [uaksen1, uaksen2]
             i += 1
             x += 1
             print(x, y)
