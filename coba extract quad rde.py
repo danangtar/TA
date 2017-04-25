bb3a780c6c8cbef3246b94ce59db498ad633b487
Danang Adi Nugroho <danang13@mhs.if.its.ac.id>
2017-04-25 14:16:01 +0700

    coba quad rde

---
+++ coba extract quad rde.py
@@ -0,0 +1,144 @@
+import numpy as np
+from scipy.io.wavfile import write
+import wave
+import binascii
+import bitarray
+import TA.pesan
+
+ba = bitarray.bitarray()
+
+spf = wave.open('testquadrde.wav', 'r')
+
+# Extract Raw Audio from Wav File
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, 'uint8')
+
+size = signal.size
+
+mod = divmod(size, 4)
+headlen = size - mod[1]
+# print(size)
+# print(mod)
+
+head = signal[:headlen]
+tail = signal[headlen:]
+print(signal[10467])
+pair = head.reshape((-1, 4))
+
+
+with open("lmquadrde", mode='rb') as file:  # b is important -> binary
+    fileContent = file.read()
+
+fileContent = list(fileContent)
+
+message = []
+
+# x = hitung sample
+x = 0
+
+# j = hitung bit pesan
+j = 0
+
+lx = []
+
+for isiquad in pair:
+    i = 0
+    uaksen = [int(isiquad[0])]
+    if j < len(fileContent):
+        while i < 3:
+            v = int(isiquad[i + 1]) - int(isiquad[i])
+            b = v & 1
+
+            vr = np.floor(v / 2)
+
+            if fileContent[j] == 0:
+                if vr > 0:
+                    v = vr + 2 ** (np.floor(np.log2(np.absolute(vr))) - 1)
+                    lx.append(0)
+                elif vr < 0:
+                    v = vr - 2 ** (np.floor(np.log2(np.absolute(vr))) - 1)
+                    lx.append(1)
+
+            elif fileContent[j] == 1:
+                if vr > 0:
+                    v = vr + 2 ** np.floor(np.log2(np.absolute(vr)))
+                    lx.append(2)
+                elif vr < 0:
+                    v = vr - 2 ** np.floor(np.log2(np.absolute(vr)))
+                    lx.append(3)
+
+            elif fileContent[j] == 2:
+                v = vr
+                lx.append(4)
+            else:
+                if 0 <= v <= 1:
+                    v = 1
+                    lx.append(5)
+                elif -2 <= v <= -1:
+                    v = -2
+                    lx.append(6)
+                else:
+                    v = 2 * np.floor(v / 2) + b
+                    lx.append(7)
+
+            uaksen.append(v + uaksen[-1])
+
+            # if fileContent[j] != 4:
+            message.append(b)
+            j += 1
+            i += 1
+
+        pair[x] = [uaksen[0], uaksen[1], uaksen[2], uaksen[3]]
+        x += 1
+
+    else:
+        break
+
+pair = np.asarray(pair, dtype=np.uint8)
+
+tulis = pair.reshape(1, -1)
+
+# print(tulis.size, head.size)
+# if tulis.size == head.size:
+#     print("ok")
+tulis = np.append(tulis, tail)
+tulis.astype(np.uint8)
+print(tulis[10467])
+write('returnquadrde.wav', 44100, tulis)
+
+# print(message)
+
+message = np.array(message, dtype=bool)
+message = message.tolist()
+lenmes = len(message)
+
+# teks = TA.pesan.Pesan()
+# teks = teks.getBinary()
+
+# print((teks > message)-(teks < message))
+
+# lmfile = open('compare.txt', 'w')
+# # str1 = bz2.compress(str1.encode("utf-8"))
+# for item in message:
+#     lmfile.write("%s," % item)
+#
+# lmfile.write("aduh")
+#
+# for item in teks:
+#     lmfile.write("%s," % item)
+#
+# lmfile.close()
+
+# print(lenmes % 8)
+
+n = int(bitarray.bitarray(message[:-(lenmes % 8)]).tostring(), 2)
+
+write = binascii.unhexlify('%x' % n)
+
+print(write)
+
+f = open('hasyilquadrde.txt', 'wb')
+f.write(write)
+f.close()
+
+print(lx.count(0), lx.count(1), lx.count(2), lx.count(3), lx.count(4), lx.count(5), lx.count(6), lx.count(7))
---
+++ coba quad rde.py
@@ -0,0 +1,160 @@
+import numpy as np
+from scipy.io.wavfile import write
+import wave
+import TA.pesan
+import pickle
+
+spf = wave.open('testttt.wav', 'r')
+
+# Extract Raw Audio from Wav File
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, 'uint8')
+
+size = signal.size
+
+mod = divmod(size, 4)
+headlen = size - mod[1]
+# print(size)
+# print(mod)
+
+head = signal[:headlen]
+tail = signal[headlen:]
+
+pair = head.reshape((-1, 4))
+y = pair.size
+pair = pair.tolist()
+
+teks = TA.pesan.Pesan()
+teks = teks.getBinary()
+panjangteks = len(teks)
+print(panjangteks)
+# print(pair)
+
+
+def expandable(v, b, u0, rde, location_map):
+    vtemp = 2 * v + b
+    if 0 <= vtemp + u0 <= 255:
+        if rde:
+            if location_map:
+                lm.append(0)
+            else:
+                lm.append(1)
+        else:
+            lm.append(2)
+        return vtemp
+    else:
+        return changeable(v, b, u0)
+
+
+def changeable(v, b, u0):
+    vtemp = 2 * np.floor(v / 2) + b
+    if 0 <= vtemp + u0 <= 255:
+        lm.append(3)
+        return vtemp
+    else:
+        return unchangeable()
+
+
+def unchangeable():
+    lm.append(4)
+    return "aduh"
+
+# x = hitung sample
+x = 0
+
+# j = hitung bit pesan
+j = 0
+
+lm = []
+# lx = []
+
+for isiquad in pair:
+    rde = False
+    location_map = False
+    i = 0
+    uaksen = [int(isiquad[0])]
+    if j < panjangteks:
+        while i < 3:
+            v = int(isiquad[i + 1]) - int(isiquad[i])
+
+            if -2 < v < 2:
+                vr = v
+                rde = False
+            elif v <= -2:
+                vr = v + 2 ** (np.floor(np.log2(np.absolute(v))) - 1)
+                rde = True
+            elif v >= 2:
+                vr = v - 2 ** (np.floor(np.log2(np.absolute(v))) - 1)
+                rde = True
+
+            if rde:
+                if 2 ** (np.floor(np.log2(np.absolute(vr)))) == 2 ** (np.floor(np.log2(np.absolute(v)))):
+                    location_map = True
+
+            if j < panjangteks:
+                b = int(teks[j])
+            else:
+                b = 0
+
+            vtemp = expandable(vr, b, int(isiquad[0]), rde, location_map)
+
+            if vtemp != "aduh":
+                uaksen.append(vtemp + uaksen[-1])
+                j += 1
+            else:
+                uaksen.append(0)
+                j += 1
+
+            i += 1
+
+        # print(j, panjangteks)
+        if 4 in lm[-3:]:
+            j -= i
+            while i > 0:
+                lm[-i] = 4
+                i -= 1
+        else:
+            pair[x] = [uaksen[0], uaksen[1], uaksen[2], uaksen[3]]
+
+        x += 1
+
+    else:
+        break
+
+
+pair = np.asarray(pair, dtype=np.uint8)
+# print(pair)
+
+tulis = pair.reshape(1, -1)
+# print(tulis.size, head.size)
+# if tulis.size == head.size:
+#     print("ok")
+tulis = np.append(tulis, tail)
+tulis.astype(np.uint8)
+# print(tulis.size, size)
+# print(tulis)
+#
+write('testquadrde.wav', 44100, tulis)
+
+# with open('lmpickle', 'wb') as lmfile:
+#     pickle.dump(lm, lmfile)
+# #
+# lmfile = open('lmungrde.txt', 'w')
+# str1 = ''.join(str(e) for e in lm)
+# # str1 = bz2.compress(str1.encode("utf-8"))
+# for item in str1:
+#     lmfile.write("%s" % item)
+#
+# lmfile.close()
+
+LMFile = open("lmquadrde", "wb")
+LMFileByteArray = bytes(lm)
+# print(LMFileByteArray)
+# print(list(LMFileByteArray))
+LMFile.write(LMFileByteArray)
+LMFile.close()
+
+# print(len(lm), panjangteks, lm.count(0), lm.count(1), lm.count(2), lx[:6])
+print(head.reshape((-1, 2)))
+# # print(head)
+# print(lm.count(2))
